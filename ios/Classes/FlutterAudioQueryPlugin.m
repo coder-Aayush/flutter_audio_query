0adb04f784ec240d49fb874a503b2d76bac19fa9
coder-Aayush <bhattaraiaayush08@gmail.com>
2023-10-22 17:10:39 +0545

    fixes

--- ios/Classes/FlutterAudioQueryPlugin.m
+++ ios/Classes/FlutterAudioQueryPlugin.m
@@ -1,6 +1,8 @@
 #import "FlutterAudioQueryPlugin.h"
+#import <MediaPlayer/MediaPlayer.h>
 
 @implementation FlutterAudioQueryPlugin
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FlutterMethodChannel* channel = [FlutterMethodChannel
       methodChannelWithName:@"flutter_audio_query"
@@ -12,9 +14,37 @@
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"getPlatformVersion" isEqualToString:call.method]) {
     result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else if ([@"getSongs" isEqualToString:call.method]) {
+    NSArray *songs = [self fetchSongs];
+    result(songs);
   } else {
     result(FlutterMethodNotImplemented);
   }
+}
+
+- (NSArray *)fetchSongs {
+  NSMutableArray *songList = [NSMutableArray array];
+  MPMediaQuery *songsQuery = [MPMediaQuery songsQuery];
+  NSArray<MPMediaItem *> *songs = [songsQuery items];
+
+  for (MPMediaItem *song in songs) {
+    NSString *title = [song valueForProperty:MPMediaItemPropertyTitle];
+    NSString *artist = [song valueForProperty:MPMediaItemPropertyArtist];
+    NSString *album = [song valueForProperty:MPMediaItemPropertyAlbumTitle];
+    NSString *uri = [song valueForProperty:MPMediaItemPropertyAssetURL];
+
+    if (title && artist && album && uri) {
+      NSDictionary *songInfo = @{
+        @"title": title,
+        @"artist": artist,
+        @"album": album,
+        @"uri": uri
+      };
+      [songList addObject:songInfo];
+    }
+  }
+
+  return songList;
 }
 
 @end
